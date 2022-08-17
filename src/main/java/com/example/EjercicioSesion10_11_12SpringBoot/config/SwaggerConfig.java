d2873770f431ef1d0df621a7a20532475df9afb6
paco <francisco.fm34@gmail.com>
2022-08-17 21:09:44 +0200

    Refactorizo los nombres adecuadamente

--- src/main/java/com/example/EjercicioSesion10_11_12SpringBoot/config/SwaggerConfig.java
+++ src/main/java/com/example/EjercicioSesion10_11_12SpringBoot/config/SwaggerConfig.java
@@ -31,6 +31,7 @@ public class SwaggerConfig {
 
     public Docket api(){
 
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiDetails())
                 .select()
